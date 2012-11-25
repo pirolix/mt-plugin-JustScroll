94ff159283dcc72ec45086711c60477868754cba
pirolix <pirolix@adbff654-c4ea-448a-8ae3-cad037306009>
2012-11-25 04:51:27 +0000

    * Ticket #15  * テンプレート書き換えを簡素に  * ソースを最新の __scaffold に合わせる  * ディレクトリ構造変更  * ライセンスを LGPL に
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/JustScroll/trunk@309 adbff654-c4ea-448a-8ae3-cad037306009

--- plugins/JustScroll/JustScroll.pl
+++ plugins/JustScroll/JustScroll.pl
@@ -1,32 +1,41 @@
 package MT::Plgin::Editing::OMV::JustScroll;
+# JustScroll (C) 2012 Piroli YUKARINOMIYA (Open MagicVox.net)
+# This program is distributed under the terms of the GNU Lesser General Public License, version 3.
 # $Id$
 
 use strict;
+use warnings;
+use MT 5;
 
-use vars qw( $VENDOR $MYNAME $VERSION );
-($VENDOR, $MYNAME) = (split /::/, __PACKAGE__)[-2, -1];
+use vars qw( $VENDOR $MYNAME $FULLNAME $VERSION $SCHEMA_VERSION );
+$FULLNAME = join '::',
+        (($VENDOR, $MYNAME) = (split /::/, __PACKAGE__)[-2, -1]);
 (my $revision = '$Rev$') =~ s/\D//g;
-$VERSION = '0.11'. ($revision ? ".$revision" : '');
+$VERSION = 'v0.12'. ($revision ? ".$revision" : '');
 
 use base qw( MT::Plugin );
 my $plugin = __PACKAGE__->new ({
-    id => $MYNAME,
-    key => $MYNAME,
+    id => $FULLNAME,
+    key => $FULLNAME,
     name => $MYNAME,
     version => $VERSION,
     author_name => 'Open MagicVox.net',
     author_link => 'http://www.magicvox.net/',
-    plugin_link => 'http://www.magicvox.net/archive/2012/05132057/', # blog
-    doc_link => 'http://lab.magicvox.net/trac/mt-plugins/wiki/JustScroll', # trac
+    plugin_link => 'http://www.magicvox.net/archive/2012/05132057/', # Blog
+    doc_link => 'http://lab.magicvox.net/trac/mt-plugins/wiki/JustScroll', # tracWiki
     description => <<'HTMLHEREDOC',
-<__trans phrase="Automatically focusing the textarea">
+<__trans phrase="Automatically focusing and resize the editing textarea">
 HTMLHEREDOC
     registry => {
-        callbacks => {
-            ### Entry/Webpage
-            'MT::App::CMS::template_source.edit_entry' => \&_hdlr_template_source_edit,
-            ### Template
-            'MT::App::CMS::template_source.edit_template' => \&_hdlr_template_source_edit,
+        applications => {
+            cms => {
+                callbacks => {
+                    # Entry/Webpage
+                    'template_source.edit_entry' => \&_hdlr_template_source_edit,
+                    # Template
+                    'template_source.edit_template' => \&_hdlr_template_source_edit,
+                },
+            },
         },
     },
 });
@@ -40,13 +49,9 @@ sub instance { $plugin; }
 sub _hdlr_template_source_edit {
     my ($cb, $app, $tmpl) = @_;
 
-    chomp (my $old = <<'MTMLHEREDOC');
-<mt:include name="include/footer.tmpl"
-MTMLHEREDOC
-    $old = quotemeta $old;
-
-    my $new = &instance->translate_templatized (<<'MTMLHEREDOC');
+    chomp (my $new = &instance->translate_templatized (<<"MTMLHEREDOC"));
 <mt:setvarblock name="jq_js_include" append="1"><mt:if name="id">
+// $FULLNAME
 // Automatically focusing the textarea for <mt:var object_type>
 <mt:if name="object_type" eq="entry">
     jQuery('#editor-header div.tab a').click (function(){
@@ -63,9 +68,20 @@ MTMLHEREDOC
         location.href = '#template-body-field';
     });
     location.href = '#template-body-field';</mt:if>
+
+    // Ticket #15 - Automatically resizing the textarea to fit against the window height
+    var justscroll_offset =
+        jQuery('#editor-content-toolbar').outerHeight() +
+        jQuery('#editor').outerHeight() +
+        3; /* Please adjust as you like */
+    function resizeEditorContentTextare () {
+        jQuery('#editor-content-textarea').height(jQuery(window).height() - justscroll_offset);
+    }
+    resizeEditorContentTextare ();
+    jQuery(window).bind ('resize', resizeEditorContentTextare);
 </mt:if></mt:setvarblock>
 MTMLHEREDOC
-    $$tmpl =~ s/($old)/$new$1/;
+    $$tmpl =~ s/^/$new/;
 }
 
 1;
